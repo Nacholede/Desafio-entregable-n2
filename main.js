2e4ad329b8dd11e1166921150c98b0588a9b8b8d
Ignacio Ledesma <nacholede013@gmail.com>
2022-07-18 00:50:55 -0300

    Hecho simulador de cambio de monedas extranjeras

---
+++ main.js
@@ -0,0 +1,43 @@
+let precioDolarBlue = 293;
+let precioEuro = 313;
+let precioReal = 23.60;
+let opcion 
+let numeroIngresado
+do {
+    alert ('Simulador de cambio a moneda extranjera')
+    opcion = prompt ('Ingrese el tipo de moneda que desea cambiar: \n1 - Dolar\n2 - Euro\n3 - Real')
+    let numeroParseado = parseFloat (numeroIngresado)
+  switch (opcion) {
+        case '1': numeroParseado = prompt ('Ingrese la cantidad de dolares a cambiar');
+        if (numeroParseado < 0) {
+        alert ('Ingrese un numero valido') ;
+        }else {
+        let resultadoDolares = numeroParseado * precioDolarBlue ;
+        alert ('La conversion es de ' + resultadoDolares + ' pesos') ;
+        break;
+    }     
+        case '2' : numeroParseado = prompt ('Ingrese la cantidad de euros a cambiar');
+        if (numeroParseado < 0) {
+            alert ('Ingrese un numero valido');
+            }else {
+            let resultadoEuros = numeroParseado * precioEuro;
+            alert ('La conversion es de ' + resultadoEuros + ' pesos');
+            break;
+        }
+        case '3' : numeroParseado = prompt ('Ingrese la cantidad de reales a cambiar')
+        if (numeroParseado < 0) {
+            alert ('Ingrese un numero valido')
+            }else {
+            let resultadoReales = numeroParseado * precioReal ;
+            alert ('La conversion es de ' + resultadoReales + ' pesos');
+            break;
+        }        
+
+
+
+ 
+
+} 
+
+
+} while (numeroIngresado !== 'FIN')
