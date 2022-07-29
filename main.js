bf5d22dbfd4f1541bb59f2baed83a3191fd91f9b
Ignacio Ledesma <nacholede013@gmail.com>
2022-07-28 21:50:20 -0300

    Agregado arrays, objetos, funciones y demas al proyecto

--- main.js
+++ main.js
@@ -1,37 +1,63 @@
-let precioDolarBlue = 301;
-let precioEuro = 313;
-let precioReal = 23.60;
+let monedas = [
+   {moneda1 : "Dolar Blue", valor1: 326},
+   {moneda2 : "Euro Blue", valor2: 341},
+   {moneda3 : "Real", valor3: 24.93},
+]
+//326=dolar blue 341=euro blue 24.93=real//
 let opcion 
 let numeroIngresado
+monedas.valor1 = 326
+monedas.valor2 = 341
+monedas.valor3 = 24.93
+
+
 do {
     alert ('Simulador de cambio de monedas extranjeras')
     opcion = prompt ('Ingrese el tipo de moneda que desea cambiar a pesos o escriba FIN para terminar la operacion: \n1 - Dolar\n2 - Euro\n3 - Real')
-    let numeroParseado = parseFloat (numeroIngresado)
+   
+    function conversionDolares () {
+        if (numeroParseado < 0 ) {
+            alert ('Ingrese un numero valido') ;
+            }else {
+            let resultado = numeroParseado * monedas.valor1 ;
+            alert ('La conversion es de ' + resultado + ' pesos') ;
+            
+        }     
+
+    }
+
+    function conversionEuros () {
+        if (numeroParseado < 0 ) {
+            alert ('Ingrese un numero valido') ;
+            }else {
+            let resultado = numeroParseado * monedas.valor2 ;
+            alert ('La conversion es de ' + resultado + ' pesos') ;
+            
+        }     
+
+    }
+
+    function conversionReales () {
+        if (numeroParseado < 0 ) {
+            alert ('Ingrese un numero valido') ;
+            }else {
+            let resultado = numeroParseado * monedas.valor3 ;
+            alert ('La conversion es de ' + resultado + ' pesos') ;
+            
+        }     
+
+    }
+
   switch (opcion) {
         case '1': numeroParseado = prompt ('Ingrese la cantidad de dolares a cambiar');
-        if (numeroParseado < 0) {
-        alert ('Ingrese un numero valido') ;
-        }else {
-        let resultadoDolares = numeroParseado * precioDolarBlue ;
-        alert ('La conversion es de ' + resultadoDolares + ' pesos') ;
+        conversionDolares()
         break;
-    }     
         case '2' : numeroParseado = prompt ('Ingrese la cantidad de euros a cambiar');
-        if (numeroParseado < 0) {
-            alert ('Ingrese un numero valido');
-            }else {
-            let resultadoEuros = numeroParseado * precioEuro;
-            alert ('La conversion es de ' + resultadoEuros + ' pesos');
-            break;
-        }
+        conversionEuros()
+        break;
         case '3' : numeroParseado = prompt ('Ingrese la cantidad de reales a cambiar')
-        if (numeroParseado < 0) {
-            alert ('Ingrese un numero valido')
-            }else {
-            let resultadoReales = numeroParseado * precioReal ;
-            alert ('La conversion es de ' + resultadoReales + ' pesos');
-            break;
-        }        
+        conversionReales ()
+        break;
 
 
 
