4912e56c2f2e05e3f92b7d6adf415f1a51b6525f
Ignacio Ledesma <nacholede013@gmail.com>
2022-08-11 18:53:26 -0300

    Cambios generales

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 
 let monedas = [
-    {moneda1 : "Dolar Blue", valor1: 326},
-    {moneda2 : "Euro Blue", valor2: 341},
-    {moneda3 : "Real", valor3: 24.93},
+    {moneda1 : 'Dolar Blue', valor1: 326},
+    {moneda2 : 'Euro Blue', valor2: 341},
+    {moneda3 : 'Real', valor3: 24.93},
  ]
  //326=dolar blue 341=euro blue 24.93=real//
  let opcion 
  let numeroIngresado
- monedas.valor1 = 291
- monedas.valor2 = 311
- monedas.valor3 = 25.45
+
  
  
 let inputDolar = document.getElementById("input-dolar")
@@ -18,10 +16,13 @@ let inputReal = document.getElementById("input-real")
 
 const cambio = document.getElementById("boton-cambio")
 
+localStorage.setItem("Monedas", JSON.stringify(monedas) )
+
+
 cambio.addEventListener('click', () => {
     conversion()
+  
 })
-
 
 
 
@@ -29,8 +30,9 @@ cambio.addEventListener('click', () => {
 function conversion () {
     if (inputDolar.checked) {
         let numeroIngresado = document.getElementById ("numero-cambio")
-        let numeroParseado = numeroIngresado.value 
-        let resultado = numeroParseado * monedas.valor1 
+        let numeroValue = numeroIngresado.value 
+        let resultado = numeroValue * monedas.valor1
+        console.log (monedas.valor1) 
         let p = document.getElementById("alerta-cambio")
         p.innerText = 'El resultado de la conversion es de ' + resultado + ' pesos'
 
@@ -38,15 +40,15 @@ function conversion () {
 
     } else if (inputEuro.checked) {
         let numeroIngresado = document.getElementById ("numero-cambio")
-        let numeroParseado = numeroIngresado.value 
-        let resultado = numeroParseado * monedas.valor2 
+        let numeroValue = numeroIngresado.value 
+        let resultado = numeroValue * monedas.valor2
         let p = document.getElementById("alerta-cambio")
         p.innerText = 'El resultado de la conversion es de ' + resultado + ' pesos'
 
     } else if (inputReal.checked) {
         let numeroIngresado = document.getElementById ("numero-cambio")
-        let numeroParseado = numeroIngresado.value 
-        let resultado = numeroParseado * monedas.valor3 
+        let numeroValue = numeroIngresado.value 
+        let resultado = numeroValue * monedas.valor3
         let p = document.getElementById("alerta-cambio")
         p.innerText = 'El resultado de la conversion es de ' + resultado + ' pesos'
 
